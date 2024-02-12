eda64347b97a56b5b66c2a93e8d3249835f4432c
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-12 17:46:24 +0000

    Added Armoury Class and tests.

--- src/main/java/Armoury.java
+++ src/main/java/Armoury.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+
 public class Armoury {
 
+    private String name;
+    private ArrayList<Weapon> weapons;
+
+    public Armoury(String name, ArrayList<Weapon> weapons){
+        this.name = name;
+        this.weapons = weapons;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public void setWeapons(ArrayList<Weapon> weapons) {
+        this.weapons = weapons;
+    }
 
 
 }
--- src/test/java/ArmouryTest.java
+++ src/test/java/ArmouryTest.java
@@ -1,12 +1,54 @@
-
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ArmouryTest {
 
-//    Armoury armoury;
-//
-//    @BeforeEach
-//    public void setUp(){
-//        armoury = new Armoury("Unlimited Bladeworks",)
-//    }
+    Armoury armoury;
+
+    Sword sword1;
+    Sword sword2;
+    Spear spear1;
+    Spear spear2;
+    ArrayList<Weapon> weapons;
+
+    @BeforeEach
+    public void setUp(){
+        sword1 = new Sword("Excalibur","Holy steel", true);
+        sword2 = new Sword("Zangetsu","Shinigami steel", false);
+
+        spear1 = new Spear("Gungnir","Norse steel, Yggdrasil wood", 1.65);
+        spear2 = new Spear("Lance of St. Longinus","Holy wood", 2.20);
+
+        weapons = new ArrayList<>();
+        weapons.add(sword1);
+        weapons.add(spear1);
+        armoury = new Armoury("Unlimited Bladeworks",weapons);
+    }
+
+    @Test
+    public void canGetName(){
+        assertThat(armoury.getName()).isEqualTo("Unlimited Bladeworks");
+    }
+    @Test
+    public void canSetName(){
+        armoury.setName("Khaz Modan Armoury");
+        assertThat(armoury.getName()).isEqualTo("Khaz Modan Armoury");
+    }
+    @Test
+    public void canGetWeapons(){
+        assertThat(armoury.getWeapons()).isEqualTo(weapons);
+    }
+
+    @Test
+    public void canSetWeapons(){
+        weapons.add(sword2);
+        armoury.setWeapons(weapons);
+        assertThat(armoury.getWeapons()).isEqualTo(weapons);
+    }
+
+    
+
 }
