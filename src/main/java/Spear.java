01e71f0957c05a044eb7f09068940a4437a9cb21
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-12 17:24:42 +0000

    Added Spear class and tests.

--- src/main/java/Spear.java
+++ src/main/java/Spear.java
@@ -1,2 +1,26 @@
-public class Spear {
+public class Spear extends Weapon{
+
+    private double length;
+
+    public Spear(String name, String material, double length){
+        super(name, material);
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public String stab(){
+        if (this.length < 2){
+            return "Size is not everything";
+        }
+        else{
+            return "Can't escape this!";
+        }
+    }
 }
--- src/test/java/SpearTest.java
+++ src/test/java/SpearTest.java
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class SpearTest {
+
+    Spear spear1;
+    Spear spear2;
+
+    @BeforeEach
+    public void setUp(){
+        spear1 = new Spear("Gungnir","Norse steel, Yggdrasil wood", 1.65);
+        spear2 = new Spear("Lance of St. Longinus","Holy wood", 2.20);
+    }
+
+    @Test
+    public void canGetLength(){
+        assertThat(spear1.getLength()).isEqualTo(1.65);
+    }
+
+    @Test
+    public void canSetLength(){
+        spear1.setLength(2);
+        assertThat(spear1.getLength()).isEqualTo(2);
+    }
+
+    @Test
+    public void canStab(){
+        assertThat(spear1.stab()).isEqualTo("Size is not everything");
+        assertThat(spear2.stab()).isEqualTo("Can't escape this!");
+    }
+
 }
